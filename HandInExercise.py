8af8ad1f78847b2a12081dc41a6b5bd9cc25492c
frlyruc <58511669+frlyruc@users.noreply.github.com>
2021-03-12 11:03:03 +0100

    Add files via upload

--- HandInExercise.py
+++ HandInExercise.py
@@ -9,10 +9,16 @@ url = "https://jobs.github.com/positions.json?"
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
         #add forms to the application
-        self.addForm('MAIN', FirstForm, name="main")
+        self.addForm('MAIN', FirstForm, name="Job_Box")
         print("app started")
         
 class FirstForm(npyscreen.ActionFormMinimal):
+
+    def printJobs(self, list):
+        jobNames = []
+        for i in range(len(list)):
+            jobNames.append("[" + list[i]['company'] + " " +list[i]['title'] + "]")
+        return jobNames
 
     def fetchBy(self, widget, list, type, typeName):
         sortedJobs = []
@@ -20,11 +26,11 @@ class FirstForm(npyscreen.ActionFormMinimal):
         if self.get_widget(widget).value:
             for i in range(len(list)):
                 if list[i][type] == typeName:
-                    sortedJobs.append("[" + list[i]['company'] + " " +list[i]['title'] + "]")
+                    sortedJobs.append(list[i])
         return sortedJobs
 
     def create(self):
-        self.add(npyscreen.Checkbox, w_id="fullTime", name = "full time?")
+        self.add(npyscreen.Checkbox, w_id="fullTime", name = "Check box if you are searching for full time jobs")
         self.add(npyscreen.TitleText, w_id="pgr", name= "Enter your preferred programming language: " )
         self.add(npyscreen.TitleText, w_id="loc", name= "Enter your preferred location: " )
 
@@ -48,8 +54,16 @@ class FirstForm(npyscreen.ActionFormMinimal):
         #print(type(resp))
         #print(resp[0]['type'])
 
+        #Sort fulltime or not
         resp1 = self.fetchBy("fullTime", resp, 'type', "Full Time")
-        npyscreen.notify_confirm(str(resp1), title="List of Companies you can work at :)", wrap=True, wide=True, editw=1)
+
+        #Sort by language
+        #resp2 = self.fetchBy("pgr", resp1, '')
+
+        #Sort by location
+        resp1 = self.fetchBy("loc", resp1, 'location', self.get_widget("loc").value)
+        resp1 = self.printJobs(resp1)
+        npyscreen.notify_confirm(str(resp1), title="List of Companies you can apply jobs at", wrap=True, wide=True, editw=1)
 
 
 
