9e02bfa0f3e54e45df5fb0b68ec0c43ebe8c3450
mspruc <55182629+mspruc@users.noreply.github.com>
2021-03-12 14:54:03 +0100

    Add files via upload

--- HandInExercise.py
+++ HandInExercise.py
@@ -3,22 +3,28 @@ import npyscreen
 import _curses
 import requests
 import time
+from npyscreen import FixedText
+from npyscreen.wgNMenuDisplay import HasMenus
 
 url = "https://jobs.github.com/positions.json?"
 
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
         #add forms to the application
-        self.addForm('MAIN', FirstForm, name="Job_Box")
+        self.addForm('MAIN', FirstForm, name="main")
         print("app started")
-        
-class FirstForm(npyscreen.ActionFormMinimal):
+ 
+class FirstForm(npyscreen.ActionFormMinimal, npyscreen.FormWithMenus):
+
+    def drawLine(self,text,relx,rely):
+        self.add(npyscreen.TitleText, w_id="sun", name=text,editable = False, relx = relx,rely = rely)
 
     def printJobs(self, list):
         jobNames = []
         for i in range(len(list)):
             jobNames.append("[" + list[i]['company'] + " " +list[i]['title'] + "]")
         return jobNames
+
 
     def fetchBy(self, widget, list, type, typeName):
         sortedJobs = []
@@ -30,14 +36,49 @@ class FirstForm(npyscreen.ActionFormMinimal):
         return sortedJobs
 
     def create(self):
-        self.add(npyscreen.Checkbox, w_id="fullTime", name = "Check box if you are searching for full time jobs")
+        #self.add(npyscreen.Checkbox, w_id="fullTime", name = "full time?")
         self.add(npyscreen.TitleText, w_id="pgr", name= "Enter your preferred programming language: " )
         self.add(npyscreen.TitleText, w_id="loc", name= "Enter your preferred location: " )
+        self.add(npyscreen.FixedText, w_id="asdawdd",  value = "Hey " )
+        test = self.add(npyscreen.ComboBox, w_id="sss", name= "test box")
+
+
+        ascii =    ("--------------------------------------------------------- -",
+                    "|                   .mmMMMMMMMMMMMMMmm.                   |",
+                    "|               .mMMMMMMMMMMMMMMMMMMMMMMMm.               |",
+                    "|            .mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.            |",
+                    "|          .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.          |",
+                    "|        .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.        |",
+                    "|       MMMMMMMM^      MMMM^^^^^^MMMM     ^MMMMMMMM       |",
+                    "|      MMMMMMMMM                           MMMMMMMMM      |",
+                    "|     MMMMMMMMMM:                         :MMMMMMMMMM     |",
+                    "|    .MMMMMMMMMM                           MMMMMMMMMM.    |",
+                    "|    MMMMMMMMM^                             ^MMMMMMMMM    |",
+                    "|    MMMMMMMMM                               MMMMMMMMM    |",
+                    "|    MMMMMMMMM                               MMMMMMMMM    |",
+                    "|    MMMMMMMMMM                             MMMMMMMMMM    |",
+                    "|    `MMMMMMMMMM                           MMMMMMMMMM`    |",
+                    "|     MMMMMMMMMMMM.                     .MMMMMMMMMMMM     |",
+                    "|      MMMMMM  MMMMMMMMMM         MMMMMMMMMMMMMMMMMM      |",
+                    "|       MMMMMM  ^MMMMMMM           MMMMMMMMMMMMMMMM       |",
+                    "|        `MMMMMM  ^MMMMM           MMMMMMMMMMMMMM`        |",
+                    "|          `MMMMMm                 MMMMMMMMMMMM`          |",
+                    "|            `^MMMMMMMMM           MMMMMMMMM^`            |",
+                    "|               `^MMMMMM           MMMMMM^`               |",
+                    "|                   `^^M           M^^`                   |",
+                    "'---------------------------------------------------------'"
+                   )
+
+        for i in range(len(ascii)):
+            self.drawLine(ascii[i],60,i)
+
+        test.values = [i,len(ascii)]
+
 
         details = {
                 'pgr' : self.get_widget("pgr").value,
-                'loc' : self.get_widget("loc").value
-            }
+                'loc' : self.get_widget("loc").value,
+           }
 
         print("buttons created")
 
